1e6b04a1b6e47a0047be7a8cfb3198414bcd8d30
Lucas Monteiro <lucashtm@id.uff.br>
2021-03-26 21:14:30 -0300

    First exercise: calculator

---
+++ calculator.py
@@ -0,0 +1,16 @@
+
+first_number = int(input("Type first number: "))
+second_number = int(input("Type second number: "))
+operation = input("Type operand (+, -, *, /): ")
+
+if operation == "+":
+    print(first_number + second_number)
+
+if operation == "-":
+    print(first_number - second_number)
+
+if operation == "*":
+    print(first_number * second_number)
+
+if operation == "/":
+    print(first_number / second_number)
