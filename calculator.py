e90ef5de8a8034b7cd561468c268c3f101f26791
Lucas Monteiro <lucashtm@id.uff.br>
2021-04-09 19:55:57 -0300

    Fix typo

--- calculator.py
+++ calculator.py
@@ -1,7 +1,6 @@
-
 first_number = int(input("Type first number: "))
 second_number = int(input("Type second number: "))
-operation = input("Type operand (+, -, *, /): ")
+operation = input("Type operation (+, -, *, /): ")
 
 if operation == "+":
     print(first_number + second_number)
