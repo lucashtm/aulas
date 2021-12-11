6bd5bd106d31d62ce4eb17d5c05ed3d6b22ccb60
Lucas Monteiro <lucashtm@id.uff.br>
2021-12-10 21:59:52 -0300

    Day Zero

---
+++ day_0/primitive_types.py
@@ -0,0 +1,11 @@
+# Integers - Inteiros
+0
+
+# Floats - Reais
+0.0
+
+# Strings - Texto
+'Jorginho Lucas'
+
+# Boolean - Booleano
+True, False
---
+++ day_0/variables.py
@@ -0,0 +1,4 @@
+integer = 5
+floating_point = 0.58
+string = 'Jorgin delicia'
+boolean = True
